e3b5c95eb757c7007964ad604444fda4b5b78072
sorabotiscrazy <122054672+sorabotiscrazy@users.noreply.github.com>
2023-01-14 22:13:49 +0000

    Update mod.py

--- mod/mod.py
+++ mod/mod.py
@@ -59,10 +59,10 @@ class Mod(ModClass):
         self._config = Config.get_conf(self, 95932766180343808, force_registration=True)
         self._config.register_guild(
             **{
-                "kick_message": "Done. That felt good.",
-                "ban_message": "Done. That felt good.",
-                "tempban_message": "Done. Enough chaos for now.",
-                "unban_message": "Unbanned that user from this server.",
+                "kick_message": "kicked {user} 🦅",
+                "ban_message": "banned {user} 🦅",
+                "tempban_message": "tempbanned {user} 🦅",
+                "unban_message": "unbanned {user} 🦅",
             }
         )
 
